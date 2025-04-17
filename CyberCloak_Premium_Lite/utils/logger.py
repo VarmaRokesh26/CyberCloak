6af9bf7755c03d8cfff83d4c09a421c2ede8452f
VarmaRokesh26 <vrokeshvarma@gmail.com>
2025-04-17 13:26:55 +0530

    Logger method

---
+++ CyberCloak_Premium_Lite/utils/logger.py
@@ -0,0 +1,25 @@
+import time
+from settings import LOG_FILE
+import tkinter as tk
+
+# This will be set by UI module on app startup
+log_console = None
+
+def set_console_reference(console_widget: tk.Text):
+    global log_console
+    log_console = console_widget
+
+def log_message(category: str, message: str):
+    timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+    log_entry = f"{timestamp} [{category}] {message}\n"
+
+    # Log to file
+    with open(LOG_FILE, "a") as log:
+        log.write(log_entry)
+
+    # Log to UI console if available
+    if log_console:
+        log_console.config(state="normal")
+        log_console.insert("end", log_entry)
+        log_console.see("end")
+        log_console.config(state="disabled")
