8237b1f9a6496f65499dde6e035571c72883837a
VarmaRokesh26 <vrokeshvarma@gmail.com>
2025-04-20 13:09:54 +0530

    Update

--- CyberCloak_Premium_Lite/utils/paths.py
+++ CyberCloak_Premium_Lite/utils/paths.py
@@ -1,9 +1,10 @@
 import os
 
-APP_DIR = r"C:\Users\ROKESH's-PC\OurVPN"
+APP_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 CONFIG_DIR = os.path.join(APP_DIR, "config")
 LOG_DIR = os.path.join(APP_DIR, "logs")
-LOG_FILE = os.path.join(LOG_DIR, "vpn_log.txt")
+LOG_FILE = os.path.join(LOG_DIR, "activity_log.txt")
 
 NMAP_PATH = r"C:\nmap\nmap.exe"
 
@@ -11,3 +12,6 @@ COMMON_PATHS = [
     r"C:\Program Files\OpenVPN\bin\openvpn.exe",
     r"C:\Program Files (x86)\OpenVPN\bin\openvpn.exe"
 ]
+
+os.makedirs(LOG_DIR, exist_ok=True)
+os.makedirs(CONFIG_DIR, exist_ok=True)
