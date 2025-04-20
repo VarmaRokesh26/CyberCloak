6b568954c0e381744482097e5bb61a457fdba536
VarmaRokesh26 <vrokeshvarma@gmail.com>
2025-04-20 12:55:25 +0530

    Updayte

--- CyberCloak_Premium_Lite/utils/logger.py
+++ CyberCloak_Premium_Lite/utils/logger.py
@@ -11,10 +11,8 @@ class Logger:
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         entry = f"{timestamp} [{category.upper()}] {message}"
 
-        # Write to file
         with open(LOG_FILE, "a", encoding="utf-8") as f:
             f.write(entry + "\n")
 
-        # Send to UI if callback is available
         if self.log_callback:
             self.log_callback(category.upper(), message)
